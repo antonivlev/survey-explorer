4f54f5080bae4d5c211de2d5eace388a765fbad9
Anton Ivlev <anton@codesigned.co.uk>
2020-01-19 12:56:18 +0000

    Add basic data fetch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SurveyExplorer from './SurveyExplorer';
-import { SURVEYS } from './data'
 
-ReactDOM.render(<SurveyExplorer surveys={SURVEYS.surveys}/>, document.getElementById('root'));
+fetch('https://my-json-server.typicode.com/focaldata/demo/db')
+  .then( res => res.json() )
+  .then( data => ReactDOM.render(<SurveyExplorer surveys={data.surveys}/>, document.getElementById('root')) )
+
+ReactDOM.render(<h3 style={{marginLeft: '20px'}}>Fetching...</h3>, document.getElementById('root'));
